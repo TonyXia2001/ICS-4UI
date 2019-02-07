73e310215e4840eda1d03973ee84e49549908179
Tony <summertony717@gmail.com>
2019-02-07 12:39:44 -0500

    added scanner

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 package testing;
 
+import java.util.*;
+
 class Main{
     public static void main(String[] args) {
         Joption a = new Joption();
         a.display();
+        Scanner s = new Scanner(System.in);
+        int i = s.nextInt();
+        System.out.println(i + 1);
     }
 }
